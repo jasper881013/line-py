280f85f3c226c45b761bf929e0ecc1f482da28c7
jasper881013 <45283667+jasper881013@users.noreply.github.com>
2018-11-23 17:03:25 +0800

    Update groupbot.py

--- examples/groupbot.py
+++ examples/groupbot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from linepy import *
 
-line = LINE('EMAIL', 'PASSWORD')
+line = LINE('jasper.j881013@msa.hinet.net', 'jasper130534097')
 #line = LINE('AUTHTOKEN')
 
 line.log("Auth Token : " + str(line.authToken))
